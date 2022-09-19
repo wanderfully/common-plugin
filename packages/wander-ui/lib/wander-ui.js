109afa16a3de4791e199e40ebd02ea99806d8644
wanderfully <1292627970@qq.com>
2022-09-19 10:50:05 +0800

    de

--- packages/wander-ui/lib/wander-ui.js
+++ packages/wander-ui/lib/wander-ui.js
@@ -1,12 +1,12 @@
 /*
- * @LastEditTime: 2022-09-16 15:21:50
+ * @LastEditTime: 2022-09-19 10:49:54
  */
 'use strict';
 module.exports = wanderUi;
 
 function wanderUi(options) {
     this.options = options
-    console.log('test plugin.....')
+    console.log('test plugin.......')
     return "Hello from wanderUi";
 }
 
